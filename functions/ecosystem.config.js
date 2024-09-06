f95f478c9be1daaa62dd11895689a91ffb9f232b
Ashishkc22 <ashishchoudhari224@gmail.com>
2024-09-06 14:35:58 +0530

    add config for render deploy

---
+++ functions/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "my-express-app", // Name of your app
+      script: "./index.js", // Path to your main application file
+      instances: 1, // Single instance for testing
+      exec_mode: "fork", // Use fork mode for a single instance
+      env: {
+        NODE_ENV: "development", // Use 'production' for production environment
+        PORT: 6060, // Port your app will run on
+      },
+    },
+  ],
+};
