c8d33c7832eb47e257c169348b9d2dda133ff9c8
Ashishkc22 <ashishchoudhari224@gmail.com>
2024-09-06 14:44:52 +0530

    c

--- functions/ecosystem.config.js
+++ functions/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "my-express-app", // Name of your app
+      name: "functions", // Name of your app
       script: "./index.js", // Path to your main application file
       instances: 1, // Single instance for testing
       exec_mode: "fork", // Use fork mode for a single instance
