c39120df0f190e185f429c9821854fb4147af60c
Ashishkc22 <ashishchoudhari224@gmail.com>
2025-04-11 08:36:44 +0530

    made changes to get-my-team-response structure.

--- functions/controllers/Cards/get-my-team-cards.js
+++ functions/controllers/Cards/get-my-team-cards.js
@@ -5,11 +5,11 @@ const { isEmpty } = require("lodash");
 const getMyTeamCards = async (req, res, next) => {
   try {
     const { tl_id } = req.userDetails;
-    const { status, page = 1, limit = 10 } = req.query;
+    const { status, page = 1, limit = 20 } = req.query;
 
     let users = await userSchema.find(
       { team_leader_id: tl_id },
-      { uid: 1, _id: 0 }
+      { uid: 1, _id: 0, image: 1 }
     );
 
     if (isEmpty(users)) {
@@ -26,13 +26,37 @@ const getMyTeamCards = async (req, res, next) => {
     const skip = (parseInt(page) - 1) * parseInt(limit);
 
     const [cards, total] = await Promise.all([
-      cardSchema.find(query).skip(skip).limit(parseInt(limit)),
+      cardSchema
+        .find(query)
+        .skip(skip)
+        .limit(parseInt(limit))
+        .populate({ path: "created_by", model: "User", select: "image _id" }),
       cardSchema.countDocuments(query),
     ]);
 
+    // Grouping the data by date
+    const groupedCardData = cards.reduce((result, doc) => {
+      const date = new Date(doc.created_at);
+      const str = `${weekName(date.getDay())} ${date.getDate()} ${monthName(
+        date.getMonth()
+      )} ${date.getFullYear()}`;
+      if (!result[str]) {
+        result[str] = {};
+        result[str].date = str;
+        result[str].count = 0;
+      }
+      if (!result[str].data) {
+        result[str].data = [];
+      }
+      result[str].count += 1;
+      doc.address = `${doc.area}, ${doc.tehsil}, ${doc.district}, ${doc.state}`;
+      result[str].data.push(doc);
+      return result;
+    }, {});
+
     res.status(200).json({
       status: "success",
-      data: cards,
+      data: groupedCardData,
       meta: {
         total,
         page: parseInt(page),
@@ -44,5 +68,55 @@ const getMyTeamCards = async (req, res, next) => {
     next(error);
   }
 };
+
+function monthName(month) {
+  switch (month) {
+    case 0:
+      return "JAN";
+    case 1:
+      return "FEB";
+    case 2:
+      return "MAR";
+    case 3:
+      return "APR";
+    case 4:
+      return "MAY";
+    case 5:
+      return "JUN";
+    case 6:
+      return "JUL";
+    case 7:
+      return "AUG";
+    case 8:
+      return "SEP";
+    case 9:
+      return "OCT";
+    case 10:
+      return "NOV";
+    case 11:
+      return "DEC";
+  }
+  return month.toString();
+}
+
+function weekName(day) {
+  switch (day) {
+    case 0:
+      return "SUN";
+    case 1:
+      return "MON";
+    case 2:
+      return "TUE";
+    case 3:
+      return "WED";
+    case 4:
+      return "THU";
+    case 5:
+      return "FRI";
+    case 6:
+      return "SAT";
+  }
+  return day.toString();
+}
 
 module.exports = getMyTeamCards;
