a317d2fe51716e06fa6f1dc38b4707eb35b9d369
Ashishkc22 <ashishchoudhari224@gmail.com>
2025-07-06 13:28:07 +0530

    made changess to update profile api validation

--- functions/controllers/User/validation.js/update-profile.validation.js
+++ functions/controllers/User/validation.js/update-profile.validation.js
@@ -1,7 +1,19 @@
 const Joi = require("joi");
-
+const currentYear = new Date().getFullYear();
 module.exports = Joi.object({
-  image: Joi.string().uri().optional(),
-  name: Joi.string().min(2).max(50).optional(),
-  phone: Joi.string().pattern(/^[0-9]{10}$/).optional(),
+  id_proof: {
+    type: Joi.string(),
+    front: Joi.string(),
+    back: Joi.string(),
+  },
+  blood_group: Joi.string(),
+  dob: Joi.number()
+    .min(currentYear - 120) // Max 120 years old
+    .max(currentYear - 10), // Min 10 years old
+  passportImage: Joi.string(),
+  address: Joi.string(),
+  state: Joi.string(),
+  district: Joi.string(),
+  emergency_contact: Joi.string().pattern(/^[6-9]\d{9}$/),
+  blood_group: Joi.string(),
 });
