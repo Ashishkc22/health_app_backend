e7f5a52bd5c9fefb4c773693885ac9392243b59f
Ashishkc22 <ashishchoudhari224@gmail.com>
2025-08-31 14:40:32 +0530

    fixed validation error.

--- functions/controllers/Cards/validation/update-card-by-id.validation.js
+++ functions/controllers/Cards/validation/update-card-by-id.validation.js
@@ -5,6 +5,7 @@ const updateSchema = Joi.object({
   image: Joi.string().uri().optional().messages({
     "string.uri": "Image must be a valid URL.",
   }),
+  uncropped_adhar_image: joi.string(),
   s_no: Joi.string().optional(),
   card_type: Joi.string().valid("Single", "Family").optional(),
   name: Joi.string().min(2).max(50).optional().messages({
